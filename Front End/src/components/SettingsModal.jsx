d0d7025e65aa13a2362d758c27563e30af1961d0
AbdooMoo <apoabdoo2018@gmail.com>
2026-05-09 12:52:54 +0300

    Update SettingsModal.jsx

--- Front End/src/components/SettingsModal.jsx
+++ Front End/src/components/SettingsModal.jsx
@@ -45,7 +45,7 @@ export default function SettingsModal({ onClose }) {
     const updated = { ...user, profileImage: "" };
     localStorage.setItem("user", JSON.stringify(updated));
     try {
-      await fetch("http://localhost:5001/api/auth/profile", {
+      await fetch("unischedule2-production.up.railway.app/api/auth/profile", {
         method: "PUT",
         headers: { "Content-Type": "application/json", Authorization: `Bearer ${token}` },
         body: JSON.stringify({ profileImage: "" })
@@ -56,7 +56,7 @@ export default function SettingsModal({ onClose }) {
   const handleSaveProfile = async () => {
     try {
       const token = localStorage.getItem("token");
-      const res = await fetch("http://localhost:5001/api/auth/profile", {
+      const res = await fetch("unischedule2-production.up.railway.app/api/auth/profile", {
         method: "PUT",
         headers: { "Content-Type": "application/json", Authorization: `Bearer ${token}` },
         body: JSON.stringify({ profileImage: profileImage })
@@ -88,7 +88,7 @@ export default function SettingsModal({ onClose }) {
     }
     try {
       const token = localStorage.getItem("token");
-      const res = await fetch("http://localhost:5001/api/auth/change-password", {
+      const res = await fetch("unischedule2-production.up.railway.app/api/auth/change-password", {
         method: "POST",
         headers: { "Content-Type": "application/json", Authorization: `Bearer ${token}` },
         body: JSON.stringify({ oldPassword: oldPass, newPassword: newPass }),
