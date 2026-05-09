9cefce699cf3c1eb749155ed9379878f87f93560
AbdooMoo <apoabdoo2018@gmail.com>
2026-05-09 03:50:29 +0300

    Update apiConfig.js

--- Front End/src/apiConfig.js
+++ Front End/src/apiConfig.js
@@ -1,2 +1,2 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5001';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'unischedule2-production.up.railway.app';
 export default API_BASE_URL;
